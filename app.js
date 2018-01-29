541532e7c877d7524fda56013f44c6c78aa748e6
syang <syang2forever@gmail.com>
2018-01-29 19:18:49 +0800

    before test

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const util = require('util');
-const async = require('async');
 const program = require('commander');
 const launcher = require('chrome-launcher');
 const CDP = require('chrome-remote-interface');
@@ -13,22 +12,8 @@ const { delay, formatDateTime } = require('./utils');
 const { recentId, fetchNewUrls, finishProfile } = require('./db');
 
 const writeFile = Promise.promisify(fs.writeFile);
-const mapLimit = function (array, limit, fn) {
-    return new Promise((resolve, reject)=>{
-        async.mapLimit(array, limit, fn, (err, results)=>{
-            if (err) {
-                reject(err);
-            } else {
-                resolve(results);
-            }
-        });
-    });
-}
 
-const mMapFile = 'maps.txt';
 let currentId = 0;
-let mUrls = [];
-let mMapFilePath = '';
 
 function writeJson(id, seq, data) {
     const path = util.format('%s/%d_%d.json', config.dst, id, seq);
@@ -40,10 +25,11 @@ program
     .option('-D --dst <path>', 'your output dst dir', './profiles')
     .option('-I --ip <host>', 'your chrome host ip', config.host)
     .option('-P --port <port>', 'your chrome debug port', config.port)
-    .option('-T --timeout <time>', 'the time to profile', 2000)
-    .option('-W --waittime <time>', 'the delay time to wait for website loading', 10000)
-    .option('-M --max <conn>', 'the maximum tab at the same time', 3)
-    .option('-B --begin <index>', 'the index of url to begin', 0);
+    .option('-T --timeout <time>', 'the time to profile', 12500)
+    .option('-W --waittime <time>', 'the delay time to wait for website loading', 12500)
+    .option('-M --max <conn>', 'the maximum tab at the same time', 5)
+    .option('-B --begin <index>', 'the index of url to begin', 0)
+    .option('-E --end <index>', '', undefined);
 
 /* profile the special url with new tab */
 async function newTab(item, timeout, delayTime) {
@@ -73,6 +59,7 @@ async function newTab(item, timeout, delayTime) {
             await Target.setAutoAttach({autoAttach: true, waitForDebuggerOnStart: false});
             Target.attachedToTarget(function (obj) {
                 if (obj.sessionId !== null) {
+                    console.log(obj.sessionId);
                     queue.push(obj.sessionId);
                 }
             });
@@ -82,6 +69,9 @@ async function newTab(item, timeout, delayTime) {
                 if (number !== undefined && message.result.profile !== undefined) {
                     writeJson(id, total, message.result.profile);
                     total++;
+                    if (writeTotal !== undefined && total > writeTotal) {
+                        finishProfile(id, total);                        
+                    }
                 }
             });
             await delay(delayTime);
@@ -90,7 +80,10 @@ async function newTab(item, timeout, delayTime) {
             const {profile} = await Profiler.stop();
             writeJson(id, 0, profile);
             let sessionId;
-            while ((sessionId=queue.pop()) !== undefined) {
+            await Promise.map(queue, async function(sessionId) {
+                if (sessionId === undefined) {
+                    return;
+                }
                 await Target.sendMessageToTarget({
                     message: JSON.stringify({id: seq, method:"Profiler.enable"}),
                     sessionId: sessionId
@@ -108,7 +101,7 @@ async function newTab(item, timeout, delayTime) {
                 });
                 map.set(seq, num++);
                 seq++;
-            }
+            }, {concurrency: 1});
         }        
     } catch (err) {
         console.error(err);
@@ -119,6 +112,7 @@ async function newTab(item, timeout, delayTime) {
                 port: config.port,
                 id: target.id
             });
+            var writeTotal = total;
             finishProfile(id, total);
         }
         if (client) {
@@ -133,7 +127,7 @@ function init() {
     config.dst = program.dst;    
     config.host = program.ip;
     config.port = program.port; 
-    currentId = program.begin;
+    currentId = parseInt(program.begin);
 
     /* 并发执行 提高效率 */
     return Promise.all([
@@ -155,12 +149,18 @@ async function main() {
         await init();
         /* run as server */
         while (true) {
-            const newId = await recentId();
+            let newId;      
+            if (program.end) {
+                newId = parseInt(program.end);
+            } else {
+                newId = await recentId();                
+            }
             if (newId >= currentId) {
                 /* fecth data */
                 const rows = await fetchNewUrls(currentId, newId);
                 /* run */
                 console.log('************ begin! ************');
+                const begintime = Date.now();
                 await Promise.map(rows, async (item) => {
                     await newTab(item, program.timeout, program.waittime);
                 }, {concurrency: program.max});
@@ -168,7 +168,9 @@ async function main() {
                     await newTab(item, program.timeout, program.waittime);
                     console.log(item.id);
                 });*/
+                const endtime = Date.now();                
                 console.log('************ end! ************');
+                console.log('the task run: %ds', (endtime-begintime)/1000);
                 currentId = newId + 1;
             } else {
                 /* delay for next request */
--- test.js
+++ test.js
@@ -1,25 +1,61 @@
-global.Promise = require("bluebird");
+const CDP = require('chrome-remote-interface');
+const launcher = require('chrome-launcher');
 
-const async = require('async');
-const {delay} = require('./utils')
+const {config} = require('./config');
+const { delay } = require('./utils');
 
-const mapLimit = function (array, limit, fn) {
-    return new Promise((resolve, reject)=>{
-        async.mapLimit(array, limit, fn, (err, results)=>{
-            if (err) {
-                reject(err);
-            } else {
-                resolve(results);
-            }
+async function newTab(url, timeout, delayTime) {
+    try {
+        // new tab
+        var target = await CDP.New({
+            host: config.host,
+            port: config.port,
+            url: url
         });
-    });
+        // profile the page
+        if (target.type === 'page') {
+            var client = await CDP({
+                host: config.host,
+                port: config.port,
+                target: target
+            });
+
+            const {Page, Target} = client;
+
+            const reply = await Target.setAutoAttach({autoAttach: true, waitForDebuggerOnStart: false});
+
+            await Page.enable();            
+            
+
+            console.log(reply);
+            
+            Target.attachedToTarget(function (obj) {
+                console.log(obj);
+                if (obj.sessionId !== null) {
+                    console.log(23333);
+                }
+            });
+
+            await delay(5000);
+
+
+        } else {
+            console.log(target);
+        } 
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (client) {
+            await client.close();
+        }
+    }
 }
 
-l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const urls = ["http://jishutx.cn/post/148.html", "http://10.131.255.32/homepage.html", "http://www.freebuf.com/", "https://github.com"];//, "https://www.drugitenovini.com/"];
 
-mapLimit(l, 3, async (item)=>{
-    await delay(4000);
-    return item;
-}).then((results)=>{
-    console.log(results);
-});
+(async ()=> {
+    await launcher.launch({port: config.port});    
+    for(let url of urls) {
+        await newTab(url, 10000, 10000);
+    }
+})();
