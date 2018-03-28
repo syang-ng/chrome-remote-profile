332761a450b049fd0df2380ac04539c4e2d4428d
syang2forever <syang2forever@gmail.com>
2018-03-29 00:55:09 +0800

    finish controller

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+from threading import Timer
 
 PKILL_CHROME = ['pkill', 'chrome']
 PKILL_NODE = ['pkill', 'node']
@@ -9,21 +10,38 @@ def generate_port_num(num):
         num = 100
     return [(100 + i) * 100 + 86 for i in range(num)]
 
-def generate_cmd(port_array):
-    return [['node', 'app.js', '-P', str(i)] for i in port_array]
+def generate_cmd(port_array, script, tabs):
+    return [['node', script, '-P', str(i), '-N', str(tabs)] for i in port_array]
+
+def kill_all():
+    subprocess.call(PKILL_NODE)
+    subprocess.call(PKILL_CHROME)
+
+def init():
+    script = 'app.js'
+    num = 3
+    tabs = 250
+    if len(sys.argv) > 1:
+        script = sys.argv[1]
+    if len(sys.argv) > 2:
+        num = int(sys.argv[2])
+    if len(sys.argv) > 3:
+        num = int(sys.argv[3])
+    return [script, num, tabs]
 
 def main():
-    num = int(sys.argv[1])
+    [script, num, tabs] = init()
     port_nums = generate_port_num(num)
-    cmds = generate_cmd(port_nums)
+    cmds = generate_cmd(port_nums, script, tabs)
     while True:
         pids = []
+        timer = Timer(600, kill_all)        
         for cmd in cmds:
             pids.append(subprocess.Popen(cmd))
+        timer.start()
         for pid in pids:
             pid.wait()
-        subprocess.call(PKILL_NODE)
-        subprocess.call(PKILL_CHROME)
+        timer.cancel()
     return
 
 if __name__ == '__main__':
