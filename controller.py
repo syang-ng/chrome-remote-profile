00a3db0ed57c19ddcd72da8bb73c2cff72ee0229
syang2forever <syang2forever@gmail.com>
2018-03-29 09:58:04 +0800

    add controller time

--- controller.py
+++ controller.py
@@ -21,22 +21,25 @@ def init():
     script = 'app.js'
     num = 3
     tabs = 250
+    timeout = 600
     if len(sys.argv) > 1:
         script = sys.argv[1]
     if len(sys.argv) > 2:
         num = int(sys.argv[2])
     if len(sys.argv) > 3:
-        num = int(sys.argv[3])
-    return [script, num, tabs]
+        tabs = int(sys.argv[3])
+    if len(sys.argv) > 4:
+        timeout = int(sys.argv[4])
+    return [script, num, tabs, timeout]
 
 def main():
-    [script, num, tabs] = init()
+    [script, num, tabs, timeout] = init()
     port_nums = generate_port_num(num)
     cmds = generate_cmd(port_nums, script, tabs)
     while True:
         print('loop begin')
         pids = []
-        timer = Timer(600, kill_all)    
+        timer = Timer(timeout, kill_all)    
         for cmd in cmds:
             print('run cmd: %s' % (' '.join(cmd)))
             pids.append(subprocess.Popen(cmd))
